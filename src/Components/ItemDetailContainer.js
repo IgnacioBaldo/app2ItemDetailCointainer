bd8cb7db9988e196b0b1b2f99dd798f52eb2247b
Ignacio <ignaciobaldo@live.com.ar>
2022-02-07 18:25:19 -0300

    consulta

--- src/Components/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Item from './Item';
 import ItemListContainer from './ItemListContainer';
 const items = [
-    {id:"01", name:"Producto 1", description:"DETALLE DEL PRODUCTO"},
+    {id:"01", name:"Producto 1", description:"DETALLE DEL PRODUCTO", price:"precio del producto $", image:""},
  
  ];
 
@@ -15,18 +15,22 @@ const ItemDetailContainer = () => {
   <h3>Producto Seleccionado para ver detalles</h3>
   <p>{selectedItem ? selectedItem.name : "No hay productos"}</p>
   <p>{selectedItem ? selectedItem.description : "No hay productos"}</p>
-  <p>ID: {selectedItem ? selectedItem.id : "No hay productos"}</p>
+  <p>{selectedItem ? selectedItem.price : "No hay productos"}</p>
+  <p>{selectedItem && selectedItem.image && <img src={selectedItem.image} alt="selectedItemImage"/>}</p>
+  
   <p>Cantidad: {selectedItem ? selectedItem.stock : "No hay productos"}</p>
 
 
 
 
   <hr/>
-  {items.map(({id, name, price, description}) =>(
+  {items.map(({id, name, price, description, image}) =>(
     <Item
     key={id}
     id={id}
     name={name}
+    price={price}
+    image={image}
     description={description}
     setSelectedItem={setSelectedItem}
     
