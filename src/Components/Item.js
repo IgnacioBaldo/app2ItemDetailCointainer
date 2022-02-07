7da80709541b375d61f28e2c196c41e48d53937d
Ignacio <ignaciobaldo@live.com.ar>
2022-02-07 18:25:08 -0300

    consulta

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -3,16 +3,18 @@ import ItemCounter from "./item-counter/itemCounter";
 
 
 
-const Item = ({id, name, description, stock, price, setSelectedItem}) =>{
+const Item = ({id, name, description, stock, image,  price, setSelectedItem}) =>{
     const [stockSelected, setStockSelected] = useState (0);
 
     const selectedItem = () =>
-        setSelectedItem ({id, name, description, price, stock: stockSelected});
+        setSelectedItem ({id, name, description, price, image, stock: stockSelected});
 
     return (
         <>
         <div>
             <h2>{name}</h2>
+            <img src={image} alt="imagen del producto" />
+            
            {/* <ItemCounter stock={stock} setStockSelected={setStockSelected} /> */}
             <button onClick={selectedItem}>Seleccionar Producto</button>
 
